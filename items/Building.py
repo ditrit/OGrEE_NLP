44969108e51aa2a727ced5e67fd34abe26a84cd9
Quentin Rabilloud <quentin.rabilloud@imt-atlantique.net>
2023-11-20 14:59:18 +0100

    Building
    Added basic methods for the Building class and fixed some bugs.

--- items/Building.py
+++ items/Building.py
@@ -1,4 +1,5 @@
 from Room import Room
+from Component import Component
 
 class Building :
     def __init__(self, name : str, position : list, rotation, size : list, template : str, rooms : list[Room] = []) :
@@ -28,5 +29,30 @@ class Building :
         string += "@".join(parameters_string)
         # TODO : add the other parameters
         return string
-    def addRoom(self):
-        pass
+    
+    def addRoom(self, name : str, position : list, rotation, size : list = None, axisOrientation : str = None, floorUnit : str = None, template : str = None, components : list[Component] = []) -> None:
+        """Adds a Room instance with specified parameters to the building."""
+        self.rooms.append(Room(".".join([self.name,name]), position, rotation, size, axisOrientation, floorUnit, template, components))
+    
+    def getIndexRoom(self, name : str) -> int:
+        """Returns the index of a room thanks to its name in the list of roomss in the building. A ValueError is raised if there
+        is no room with such name"""
+        k = 0
+        n = len(self.rooms) - 1
+        if n < 0:
+            raise IndexError("There is no building in this site.")
+        while k < n or (name != self.rooms[k].name and ".".join([self.name,name]) != self.rooms[k].name):
+            k += 1
+        if k == n + 1:
+            raise ValueError("The room {} does not exist.".format(name))
+        return k
+
+    def getRoom(self,name : str) -> Room:
+        """Returns a Room instance from the building thanks to its name. A ValueError is raised if there is no room with such name."""
+        return self.rooms[self.getIndexRoom(name)]
+    
+    def removeRoom(self, name : str) -> None:
+        """Removes a Room instance from the building thanks to its name. A ValueError is raised if there is no room with such name.
+        This operation is final and means that the Room instance is permanently deleted."""
+        del self.rooms[self.getIndexRoom(name)]
+        
--- items/Site.py
+++ items/Site.py
@@ -10,26 +10,28 @@ class Site :
         return str(self.name)
     
     def addBuilding(self, name : str, position : list, rotation, size : list, template : str, rooms : list[Room] = []) -> None:
-        """Adds a Building instance with specified parameters to the Site."""
+        """Adds a Building instance with specified parameters to the site."""
         self.buildings.append(Building(".".join([self.name,name]), position, rotation, size, template, rooms))
     
-    def getIndexBuilding(self, name : str):
+    def getIndexBuilding(self, name : str) -> int:
+        """Returns the index of a Building thanks to its name in the list of buildings located on the site. A ValueError is raised if there
+        is no building with such name."""
         k = 0
         n = len(self.buildings) - 1
         if n < 0:
             raise IndexError("There is no building in this site.")
-        while k < n or name != self.buildings[k].name:
+        while k < n or (name != self.buildings[k].name and ".".join([self.name, name]) != self.buildings[k].name):
             k += 1
         if k == n + 1:
             raise ValueError("The building {} does not exist.".format(name))
         return k
 
     def getBuilding(self,name : str) -> Building:
-        """Returns a Building instance from a Site thanks to its name. A ValueError is raised if there is no building with such name."""
+        """Returns a Building instance from the site thanks to its name. A ValueError is raised if there is no building with such name."""
         return self.buildings[self.getIndexBuilding(name)]
     
     def removeBuilding(self, name : str) -> None:
-        """Removes a Building instance from a Site thanks to its name. A ValueError is raised if there is no building with such name.
+        """Removes a Building instance from the site thanks to its name. A ValueError is raised if there is no building with such name.
         This operation is final and means that the Building instance is permanently deleted."""
         del self.buildings[self.getIndexBuilding(name)]
         
@@ -38,6 +40,7 @@ if __name__ == "__main__":
     site.addBuilding("B1", [2,5], 0, [8,9,2], "")
     print(site.buildings)
     print(site.getBuilding("S1.B1"))
+    print(site.getBuilding("B1"))
     site.removeBuilding("S1.B1")
     print(site.buildings)
     print(site.getBuilding("S1.B1"))
