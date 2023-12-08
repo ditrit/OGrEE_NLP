ca018b53e9c0452ae1ae5e618ee7811dffdfac1f
Nyosema <nino.alapetite@gmail.com>
2023-12-08 16:09:54 +0100

    testj'en ai marre

--- ogree_wiki.py
+++ ogree_wiki.py
@@ -165,12 +165,12 @@ class Room :
     def addPillar(self, name : str, center : list, size : list, rotation : int) -> None:
         #TO DO : Quentin
         pass
-
+"""
 class Rack:
     rotation_possible = {"LEFT" : [0,90,0], "RIGHT": [0,-90,0], "FRONT" : [0,0,180], "REAR": [0,0,0] , "TOP": [90,0,0],
     "BOTTOM": [-90,0,0]}
 
-    def __init__(self, position : list, unit : str, rack_rotation : str | list, size : list : None, template : str = None):
+    def __init__(self, position : list, unit : str, rack_rotation : str | list, size : list | None, template : str = None):
         self.position = position
         self.unit = unit
         self.rack_rotation = rack_rotation
@@ -188,8 +188,7 @@ class Rack:
             boolean = boolean and (type(coor) in [float,int])
         #We check the rotation. It should be a str which is a key of rotation_possible or a list in the values
         # of rotation_possible
-        boolean = boolean and (rack_rotation in rotation_possible.keys() or rack_rotation in rotation_possible.values())
-
+        boolean = boolean and (rack_rotation in rotation_possible.keys() or rack_rotatiotation_possible.values())
         #We should have a size XOR a template
         if (self.size != None or self.template !=None) and not(self.size !=None and self.template !=None) :
 
@@ -207,6 +206,6 @@ class Rack:
         else: 
             boolean = False
         
-        return boolean
+        return boolean"""
 
 
